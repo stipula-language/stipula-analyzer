ad8d3e1ef2dd8df54475c2f2c6c3f15b5cf791b8
Samuele Evangelisti <samueleevangelisti@yahoo.it>
2024-01-23 19:19:34 +0100

    .

--- analyzer.py
+++ analyzer.py
@@ -37,15 +37,16 @@ def _clear_code(line_list):
 
 def _print_code(line_list, visitor_entry, is_readable):
     line_str = f"line {visitor_entry.code_reference.start_line}:   "
-    print(f"=== {(visitoroutputprints.str_visitor_entry(visitor_entry) if is_readable else str(visitor_entry))} ===")
+    print(f"    {(visitoroutputprints.str_visitor_entry(visitor_entry) if is_readable else str(visitor_entry))} :")
     print(f"{line_str}{(' ' * len(line_str)).join(_clear_code(line_list[visitor_entry.code_reference.start_line - 1:visitor_entry.code_reference.end_line]))}")
 
 
 
 @click.command()
 @click.option('-r', '--readable', 'is_readable', type=bool, default=False, show_default=True, is_flag=True, help='Show the output human readable.')
+@click.option('-c', '--compact', 'is_compact', type=bool, default=False, show_default=True, is_flag=True, help='Show only the relevant output.')
 @click.argument('file-path', type=str)
-def _main(is_readable, file_path):
+def _main(is_readable, is_compact, file_path):
     input_stream = antlr4.FileStream(file_path)
     lexer = StipulaLexer(input_stream)
     stream = antlr4.CommonTokenStream(lexer)
@@ -61,32 +62,38 @@ def _main(is_readable, file_path):
     print('+-------------+')
     print('|   Results   |')
     print('+-------------+')
-    print()
     if is_readable:
-        visitoroutputprints.print_visitor_output(visitor_output)
+        visitoroutputprints.print_visitor_output(visitor_output, is_compact)
     else:
         print(visitor_output)
 
     if visitor_output.warning_constraint:
         print('WARNING CONSTRAINT')
         for dependency_tuple_1, dependency_tuple_2 in visitor_output.warning_constraint:
-            print(f"{' + '.join(dependency_tuple_1)} <= {(' + '.join(dependency_tuple_2) if dependency_tuple_2 else '0')}")
+            print(f"    {' + '.join(dependency_tuple_1)} <= {(' + '.join(dependency_tuple_2) if dependency_tuple_2 else '0')}")
         print('==================')
     if visitor_output.warning_code:
         print('WARNING CODE')
         for visitor_entry_1, visitor_entry_2 in visitor_output.warning_code:
-            print(f"{(visitoroutputprints.str_visitor_entry(visitor_entry_1) if is_readable else str(visitor_entry_1))} --X-> {(visitoroutputprints.str_visitor_entry(visitor_entry_2) if is_readable else str(visitor_entry_2))}")
-            _print_code(line_list, visitor_entry_1, is_readable)
-            _print_code(line_list, visitor_entry_2, is_readable)
+            print(f"    {(visitoroutputprints.str_visitor_entry(visitor_entry_1) if is_readable else str(visitor_entry_1))} --X-> {(visitoroutputprints.str_visitor_entry(visitor_entry_2) if is_readable else str(visitor_entry_2))}")
+            if not is_compact:
+                _print_code(line_list, visitor_entry_1, is_readable)
+                _print_code(line_list, visitor_entry_2, is_readable)
         print('============')
     if visitor_output.expired_code:
         print('EXPIRED CODE')
         for visitor_entry in visitor_output.expired_code:
+            if is_compact:
+                print(f"    {visitoroutputprints.str_visitor_entry(visitor_entry)}")
+                continue
             _print_code(line_list, visitor_entry, is_readable)
         print('============')
     if visitor_output.dead_code:
         print('DEAD CODE')
         for visitor_entry in visitor_output.dead_code:
+            if is_compact:
+                print(f"    {visitoroutputprints.str_visitor_entry(visitor_entry)}")
+                continue
             _print_code(line_list, visitor_entry, is_readable)
         print('=========')
 
--- utils/visitoroutputprints.py
+++ utils/visitoroutputprints.py
@@ -24,21 +24,33 @@ def str_visitor_entry(visitor_entry):
 
 
 
-def print_visitor_output(visitor_output):
+def print_visitor_output(visitor_output, is_compact):
     print(json.dumps({
         'field_id_set': list(visitor_output.field_id_set),
         'Q0': visitor_output.Q0,
         'C': [str_visitor_entry(visitor_entry) for visitor_entry in visitor_output.C],
-        'Gamma': {str_visitor_entry(event_visitor_entry): str_visitor_entry(function_visitor_entry) for event_visitor_entry, function_visitor_entry in visitor_output.Gamma.items()},
-        'dependency_t_dict': {str_visitor_entry(visitor_entry): list(field_id_set) for visitor_entry, field_id_set in visitor_output.dependency_t_dict.items()},
-        't': {str_visitor_entry(visitor_entry): str(time_delta) for visitor_entry, time_delta in visitor_output.t.items()},
-        'T': {str_visitor_entry(visitor_entry): {
+        **({
+            'Gamma': {str_visitor_entry(event_visitor_entry): str_visitor_entry(function_visitor_entry) for event_visitor_entry, function_visitor_entry in visitor_output.Gamma.items()}
+        } if not is_compact else {}),
+        **({
+            'dependency_t_dict': {str_visitor_entry(visitor_entry): list(field_id_set) for visitor_entry, field_id_set in visitor_output.dependency_t_dict.items()}
+        } if not is_compact else {}),
+        **({
+            't': {str_visitor_entry(visitor_entry): str(time_delta) for visitor_entry, time_delta in visitor_output.t.items()}
+        } if not is_compact else {}),
+        'T': {str_visitor_entry(visitor_entry): ({
             'value': str(value_dependency.value),
             'dependency_set': list(value_dependency.dependency_set)
-        } for visitor_entry, value_dependency in visitor_output.T.items()},
+        } if not is_compact else str(value_dependency.value)) for visitor_entry, value_dependency in visitor_output.T.items()},
         'R': [str_visitor_entry(visitor_entry) for visitor_entry in visitor_output.R],
-        'warning_constraint': [f"{field_id_1} <= {field_id_2}" for field_id_1, field_id_2 in visitor_output.warning_constraint],
-        'warning_code': [f"{str_visitor_entry(visitor_entry_1)} -> {str_visitor_entry(visitor_entry_2)}" for visitor_entry_1, visitor_entry_2 in visitor_output.warning_code],
-        'expired_code': {str_visitor_entry(visitor_entry): date_str for visitor_entry, date_str in visitor_output.expired_code.items()},
+        **({
+            'warning_constraint': [f"{field_id_1} <= {field_id_2}" for field_id_1, field_id_2 in visitor_output.warning_constraint]
+        } if not is_compact else {}),
+        **({
+            'warning_code': [f"{str_visitor_entry(visitor_entry_1)} -> {str_visitor_entry(visitor_entry_2)}" for visitor_entry_1, visitor_entry_2 in visitor_output.warning_code]
+        } if not is_compact else {}),
+        **({
+            'expired_code': {str_visitor_entry(visitor_entry): date_str for visitor_entry, date_str in visitor_output.expired_code.items()}
+        } if not is_compact else {}),
         'dead_code': [str_visitor_entry(visitor_entry) for visitor_entry in visitor_output.dead_code]
     }, indent=2))
