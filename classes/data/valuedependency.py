0b783171a7ac8a1f6938194817fb17b7a3d515cd
Samuele Evangelisti <samueleevangelisti@yahoo.it>
2024-03-02 18:20:38 +0100

    .

--- classes/data/valuedependency.py
+++ classes/data/valuedependency.py
@@ -13,7 +13,7 @@ class ValueDependency:
 
     def __eq__(self, value_dependency):
         return self.value == value_dependency.value and self.dependency_tuple == value_dependency.dependency_tuple
-    
+
 
 
     def __hash__(self):
